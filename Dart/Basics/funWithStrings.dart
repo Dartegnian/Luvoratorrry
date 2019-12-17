1a464623b8284920f2c6044d90a8fbf18e49af56
Dartegnian <Dartegnian@users.noreply.github.com>
2019-12-18 02:48:06 +0800

    Added strings program

---
+++ Dart/Basics/funWithStrings.dart
@@ -0,0 +1,13 @@
+void main() {
+  String singleLineString = "Waddup";
+  String multiLineString = """
+  Unknown
+  Mother
+  Goose
+  """;
+  String rawString = r"Waddup it's me... \n Mario!";
+
+  print(singleLineString);
+  print(multiLineString);
+  print(rawString);
+}
