7ca5c467d9f8442231d2d4664fc0e76426d7ff18
Dartegnian <Dartegnian@users.noreply.github.com>
2020-01-26 02:05:09 +0800

    Placed header into component

---
+++ Vue/vue-caloid/src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<header class="vuecaloids__header">
+		<h1 class="vocaloids__title">Vocaloids in Vue</h1>
+		<router-link to="/">Home</router-link> |
+		<router-link to="/about">About</router-link>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header"
+}
+</script>
+
+<style>
+</style>
